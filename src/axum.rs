964a0115d5a1c277ae7570b42c59f17683a71871
timtom2016 <dev@timtom2016.com>
2024-08-29 19:03:18 +0200

    First Iteration of Server and ClientSignals done

--- src/axum.rs
+++ src/axum.rs
@@ -1,29 +1,63 @@
-use std::{collections::HashMap, sync::{Arc}};
+use std::{collections::HashMap, sync::Arc};
 
-use axum::{async_trait, extract::{ws::Message, State}};
-use futures::{future::BoxFuture, stream::{select_all, SplitSink}, Future, SinkExt, StreamExt};
-use leptos::reactive_graph::{effect::Effect, owner::expect_context};
-use tokio::{spawn, sync::{broadcast::{channel, error::RecvError, Receiver, Sender}, Mutex, RwLock}, task::JoinSet};
-use crate::{messages::Messages, server_signals::{self, ServerSignals}, messages::ServerSignalUpdate};
+use crate::{
+    messages::Messages,
+    messages::ServerSignalUpdate,
+    server_signals::{self, ServerSignals},
+};
+use axum::{
+    async_trait,
+    extract::{ws::Message, State},
+};
+use futures::{
+    future::BoxFuture,
+    stream::{select_all, SplitSink},
+    Future, SinkExt, StreamExt,
+};
+use leptos::{
+    prelude::warn,
+    reactive_graph::{effect::Effect, owner::expect_context},
+};
+use tokio::{
+    spawn,
+    sync::{
+        broadcast::{channel, error::RecvError, Receiver, Sender},
+        Mutex, RwLock,
+    },
+    task::JoinSet,
+};
 
-pub async fn handle_broadcasts(mut receiver: Receiver<ServerSignalUpdate>, sink: Sender<Message>) {
+pub async fn handle_broadcasts(
+    mut receiver: Receiver<ServerSignalUpdate>,
+    sink: Arc<RwLock<SplitSink<axum::extract::ws::WebSocket, axum::extract::ws::Message>>>,
+) {
     while let Ok(message) = receiver.recv().await {
-        if sink.send(Message::Text(serde_json::to_string(&message).unwrap())).is_err() {
+        if sink
+            .write()
+            .await
+            .send(Message::Text(
+                serde_json::to_string(&Messages::Update(message)).unwrap(),
+            ))
+            .await
+            .is_err()
+        {
             break;
         };
     }
 }
 
-pub async fn websocket(ws: axum::extract::WebSocketUpgrade,State(server_signals): State<ServerSignals>) -> axum::response::Response {
-    ws.on_upgrade(move |socket| handle_socket(socket,server_signals))
+pub async fn websocket(
+    ws: axum::extract::WebSocketUpgrade,
+    State(server_signals): State<ServerSignals>,
+) -> axum::response::Response {
+    ws.on_upgrade(move |socket| handle_socket(socket, server_signals))
 }
 
-async fn handle_socket(mut socket: axum::extract::ws::WebSocket,server_signals: ServerSignals) {
-    let (mut send,mut recv) = socket.split();
-    let (websocket_sender,websocket_receiver) = channel(10);
+async fn handle_socket(mut socket: axum::extract::ws::WebSocket, server_signals: ServerSignals) {
+    let (mut send, mut recv) = socket.split();
+    let send = Arc::new(RwLock::new(send));
     let server_signals2 = server_signals.clone();
     spawn(async move {
-        
         while let Some(message) = recv.next().await {
             if let Ok(msg) = message {
                 match msg {
@@ -31,23 +65,25 @@ async fn handle_socket(mut socket: axum::extract::ws::WebSocket,server_signals: 
                         let message: Messages = serde_json::from_str(&text).unwrap();
                         match message {
                             Messages::Establish(name) => {
-                                let mut recv = server_signals.add_observer(name.clone()).await.unwrap();
-                               spawn(handle_broadcasts(recv, websocket_sender.clone()));
-
-                            },
-                            Messages::Update(update) => {server_signals.update(update.name.to_string(), update).await.unwrap();},
+                                let mut recv =
+                                    server_signals.add_observer(name.clone()).await.unwrap();
+                                spawn(handle_broadcasts(recv, send.clone()));
+                            }
+                            Messages::Update(update) => {
+                                error!("You can't change the server signal from the client side")
+                            }
                         }
-                    },
+                    }
                     Message::Binary(_) => todo!(),
                     Message::Ping(_) => todo!(),
                     Message::Pong(_) => todo!(),
-                    Message::Close(_) => todo!(),
+                    Message::Close(_) => {}
                 }
             } else {
                 break;
             }
-            
-        };
-        
-    }).await.unwrap();
+        }
+    })
+    .await
+    .unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,169 @@
+#[cfg(not(feature = "ssr"))]
+use client_signals::ClientSignals;
 use json_patch::Patch;
-use leptos::reactive_graph::signal::ArcRwSignal;
+use leptos::*;
+use messages::{Messages, ServerSignalUpdate};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::{
     any::{Any, TypeId},
     borrow::Cow,
+    cell::RefCell,
+    collections::HashMap,
+    rc::Rc,
+    sync::{Arc, Mutex},
 };
 use wasm_bindgen::JsValue;
 use web_sys::WebSocket;
 
-pub mod messages;
 pub mod error;
-#[cfg(feature="ssr")]
+pub mod messages;
+#[cfg(feature = "ssr")]
 pub mod server_signal;
 
 #[cfg(feature = "ssr")]
 pub mod server_signals;
 
-#[cfg(all(feature = "axum", feature = "ssr"))]
-mod axum;
+#[cfg(not(feature = "ssr"))]
+pub mod client_signal;
 
-#[cfg(target_arch = "wasm32")]
+#[cfg(not(feature = "ssr"))]
+pub mod client_signals;
+
+#[cfg(all(feature = "axum", feature = "ssr"))]
+pub mod axum;
+#[cfg(not(feature = "ssr"))]
+#[derive(Clone)]
+pub struct ServerSignalWebSocket {
+    ws: send_wrapper::SendWrapper<WebSocket>,
+    // References to these are kept by the closure for the callback
+    // onmessage callback on the websocket
+    state_signals: ClientSignals,
+    // When the websocket is first established, the leptos may not have
+    // completed the traversal that sets up all of the state signals.
+    // Without that, we don't have a base state to apply the patches to,
+    // and therefore we must keep a record of the patches to apply after
+    // the state has been set up.
+    delayed_updates: Arc<Mutex<HashMap<String, Vec<ServerSignalUpdate>>>>,
+    delayed_msgs: Arc<Mutex<Vec<Messages>>>,
+}
+#[cfg(not(feature = "ssr"))]
+impl ServerSignalWebSocket {
+    /// Returns the inner websocket.
+    pub fn ws(&self) -> WebSocket {
+        self.ws.clone().take()
+    }
+    pub fn send(&self, msg: &Messages) -> Result<(), serde_json::Error> {
+        let serialized_msg = serde_json::to_string(&msg)?;
+        if self.ws.ready_state() != WebSocket::OPEN {
+            self.delayed_msgs.lock().unwrap().push(msg.clone());
+        } else {
+            self.ws
+                .send_with_str(&serialized_msg)
+                .expect("Failed to send message");
+        }
+        Ok(())
+    }
+}
+
+#[cfg(not(feature = "ssr"))]
 #[inline]
 fn provide_websocket_inner(url: &str) -> Result<Option<WebSocket>, JsValue> {
-    use js_sys::{Function, JsString};
-    use leptos::{use_context, SignalUpdate};
+    use std::time::Duration;
+
+    use leptos::prelude::{provide_context, use_context};
+    use prelude::{set_timeout, warn};
     use wasm_bindgen::{prelude::Closure, JsCast};
+    use web_sys::js_sys::{Function, JsString};
     use web_sys::MessageEvent;
 
-    if use_context::<ServerSignalWebSocket>().is_none() {
-        let ws = WebSocket::new(url)?;
+    if let None = use_context::<ServerSignalWebSocket>() {
+        let ws = send_wrapper::SendWrapper::new(WebSocket::new(url)?);
+        ws.set_binary_type(web_sys::BinaryType::Arraybuffer);
         provide_context(ServerSignalWebSocket {
             ws,
-            state_signals: Rc::default(),
-            delayed_updates: Rc::default(),
+            state_signals: ClientSignals::new(),
+            delayed_updates: Arc::default(),
+            delayed_msgs: Arc::default(),
         });
     }
 
-    let ws = use_context::<ServerSignalWebSocket>().unwrap();
+    match use_context::<ServerSignalWebSocket>() {
+        Some(ws) => {
+            let handlers = ws.state_signals.clone();
+            provide_context(ws.state_signals.clone());
+            let delayed_updates = ws.delayed_updates.clone();
 
-    let handlers = ws.state_signals.clone();
-    let delayed_updates = ws.delayed_updates.clone();
+            let callback = Closure::wrap(Box::new(move |event: MessageEvent| {
+                let ws_string = event
+                    .data()
+                    .dyn_into::<JsString>()
+                    .unwrap()
+                    .as_string()
+                    .unwrap();
 
-    let callback = Closure::wrap(Box::new(move |event: MessageEvent| {
-        let ws_string = event
-            .data()
-            .dyn_into::<JsString>()
-            .unwrap()
-            .as_string()
-            .unwrap();
-        if let Ok(update_signal) = serde_json::from_str::<ServerSignalUpdate>(&ws_string) {
-            let handler_map = (*handlers).borrow();
-            let name = &update_signal.name;
-            let mut delayed_map = (*delayed_updates).borrow_mut();
-            if let Some(signal) = handler_map.get(name) {
-                if let Some(delayed_patches) = delayed_map.remove(name) {
-                    signal.update(|doc| {
-                        for patch in delayed_patches {
-                            json_patch::patch(doc, &patch).unwrap();
+                match serde_json::from_str::<Messages>(&ws_string) {
+                    Ok(Messages::Establish(_)) => todo!(),
+                    Ok(Messages::Update(update)) => {
+                        let name = &update.name;
+                        let mut delayed_map = (*delayed_updates).lock().unwrap();
+                        if let Some(delayed_patches) = delayed_map.remove(&name.to_string()) {
+                            for patch in delayed_patches {
+                                handlers.update(name.to_string(), patch);
+                            }
                         }
-                    });
+                        handlers.update(name.to_string(), update);
+                        // delayed_map
+                        //     .entry(name.clone())
+                        //     .or_default()
+                        //     .push(update_signal.patch.clone());
+                    }
+                    Err(err) => {
+                        warn!("Couldn't deserialize Socket Message {}", err)
+                    }
                 }
-                signal.update(|doc| {
-                    json_patch::patch(doc, &update_signal.patch).unwrap();
-                });
-            } else {
-                leptos::logging::warn!("No local state for update to {}. Queuing patch.", name);
-                delayed_map
-                    .entry(name.clone())
-                    .or_default()
-                    .push(update_signal.patch.clone());
-            }
+            }) as Box<dyn FnMut(_)>);
+            let mut ws2 = ws.clone();
+            let onopen_callback = Closure::<dyn FnMut()>::new(move || {
+                if let Ok(mut delayed_msgs) = ws2.delayed_msgs.lock() {
+                    for msg in delayed_msgs.drain(..) {
+                        if let Err(err) = ws2.send(&msg) {
+                            eprintln!("Failed to send delayed message: {:?}", err);
+                        }
+                    }
+                }
+            });
+            let function: &Function = callback.as_ref().unchecked_ref();
+            ws.ws.set_onmessage(Some(function));
+            ws.ws
+                .set_onopen(Some(onopen_callback.as_ref().unchecked_ref()));
+            onopen_callback.forget();
+            // Keep the closure alive for the lifetime of the program
+            callback.forget();
+
+            Ok(Some(ws.ws()))
         }
-    }) as Box<dyn FnMut(_)>);
-    let function: &Function = callback.as_ref().unchecked_ref();
-    ws.ws.set_onmessage(Some(function));
-
-    // Keep the closure alive for the lifetime of the program
-    callback.forget();
-
-    Ok(Some(ws.ws()))
+        None => todo!(),
+    }
 }
 
-#[cfg(not(target_arch = "wasm32"))]
+#[cfg(feature = "ssr")]
 #[inline]
-fn provide_websocket_inner(_url: &str) -> Result<Option<WebSocket>, JsValue> {
+fn provide_websocket_inner(url: &str) -> Result<Option<WebSocket>, JsValue> {
     use wasm_bindgen::JsValue;
     use web_sys::WebSocket;
 
     Ok(None)
 }
 
+pub fn provide_websocket(url: &str) -> Result<Option<WebSocket>, JsValue> {
+    provide_websocket_inner(url)
+}
+
 /// A server signal update containing the signal type name and json patch.
 ///
 /// This is whats sent over the websocket, and is used to patch the signal if the type name matches.
-
-
-#[derive(Clone, Serialize,Deserialize)]
-pub struct History(pub Vec<f64>);
-
-impl Into<ArcRwSignal<History>> for History {
-    fn into(self) -> ArcRwSignal<History> {
-        ArcRwSignal::new(self)
-    }
-}
-
+#[cfg(feature = "ssr")]
 #[cfg(test)]
 mod test {
     use std::{time::Duration, vec};
@@ -113,9 +172,20 @@ mod test {
     use leptos::prelude::*;
     use tokio::{spawn, task, time::sleep};
     use web_sys::js_sys::Math::sign;
+    #[derive(Clone, Serialize, Deserialize)]
+    pub struct History(pub Vec<f64>);
+
+    impl Into<ArcRwSignal<History>> for History {
+        fn into(self) -> ArcRwSignal<History> {
+            ArcRwSignal::new(self)
+        }
+    }
 
     use crate::{
-        error, server_signal::{ServerSignal, ServerSignalTrait}, server_signals::{self, ServerSignals}, History
+        error,
+        server_signal::{ServerSignal, ServerSignalTrait},
+        server_signals::{self, ServerSignals},
+        History,
     };
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 2)]
@@ -127,8 +197,7 @@ mod test {
         let server_signals = ServerSignals::new();
         provide_context(server_signals.clone());
         use_context::<ServerSignals>().unwrap();
-        let signal = ServerSignal::new(history.clone(),History(vec![1.0, 2.0, 3.0, 4.0]))
-            .unwrap();
+        let signal = ServerSignal::new(history.clone(), History(vec![1.0, 2.0, 3.0, 4.0])).unwrap();
         let text = RwSignal::new("".to_string());
         let signal2 = signal.clone();
         signal2.update(|values| values.0.push(5.0));
@@ -145,20 +214,17 @@ mod test {
         let server_signals = ServerSignals::new();
         provide_context(server_signals.clone());
         use_context::<ServerSignals>().unwrap();
-        let signal = ServerSignal::new(history.clone(),History(vec![1.0, 2.0, 3.0, 4.0]))
-            .unwrap();
+        let signal = ServerSignal::new(history.clone(), History(vec![1.0, 2.0, 3.0, 4.0])).unwrap();
         let mut observer = signal.add_observer().await;
         let signal2 = signal.clone();
         signal2.update(|values| values.0.push(5.0));
         Executor::tick().await;
-        println!("{:?}",observer.recv().await);
+        println!("{:?}", observer.recv().await);
         signal2.update(|values| values.0.push(5.0 + 1 as f64));
         Executor::tick().await;
-        println!("{:?}",observer.recv().await);
+        println!("{:?}", observer.recv().await);
         signal2.update(|values| values.0.push(5.0 + 2 as f64));
         Executor::tick().await;
-        println!("{:?}",observer.recv().await);
-        
-        
+        println!("{:?}", observer.recv().await);
     }
 }
